db493e108fcae6c4004f54801bd5a16b5ac7d02a
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2022-02-10 00:39:43 +0530

    Let's say hello to modern UI/UX😊✨🤘

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import gpt3Logo from '../../logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>DevAshu AKA Ashutosh102, <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Ashutosh Mohanty, Tankpani Road BBSR</p>
+        <p>091-7749067820</p>
+        <p>ashutoshmohanty3815@gmail.com</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2022 DEVTECH. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
